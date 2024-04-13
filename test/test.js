9b8ffcb1af8abaf03c07f209ec4fa62e95e637e8
wuyu <wuyu.code@gmail.com>
2024-04-14 02:05:26 +0800

    '"', "'", '`' must appear in pairs, while other quotation marks end upon encountering a '"'

--- test/test.js
+++ test/test.js
@@ -11,11 +11,12 @@ function testFix() {
     assert.strictEqual(fix(']'), '');
     assert.strictEqual(fix(':'), '');
     assert.strictEqual(fix('{"a": 1}'), '{"a":1}');
+    assert.strictEqual(fix("{'a': 1}"), '{"a":1}');
     assert.strictEqual(fix('{"a":: 1}'), '{"a":1}');
-    assert.strictEqual(fix('{"a": 1]'), '{"a":1}');
+    assert.strictEqual(fix('{"a": 1，\'b\': 2, `c`: 3, “d”: 4, ‘e’: 5, 「f」：6, ·g·: 7}'), '{"a":1,"b":2,"c":3,"d":4,"e":5,"f":6,"g":7}');
     assert.strictEqual(fix('{"a": 1, {"b": 2]]'), '{"a":1,{"b":2}}');
     assert.strictEqual(fix('{,,,"a",,:, 1,,, {,,,"b",: 2,,,],,,],,,'), '{"a":1,{"b":2}}');
-    assert.strictEqual(fix('{"a": 1, b: [2, “3":}]'), '{"a":1,b:[2,"3"]}');
+    assert.strictEqual(fix('{"a": 1, b: [2, “3”:}]'), '{"a":1,b:[2,"3"]}');
     assert.strictEqual(fix('{"a": 1, b:: [2, “3":}] // this is a comment'), '{"a":1,b:[2,"3"]}');
     assert.strictEqual(fix('},{,"key":： “value"，】, // this is a comment'), '{"key":"value"}');
 }
