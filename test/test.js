449096cde520933163798ffc3fe04e08be39dbff
wuyu <wuyu.code@gmail.com>
2024-04-13 20:54:57 +0800

    Simplify StringIterator.next()

--- test/test.js
+++ test/test.js
@@ -5,6 +5,10 @@ function testFix() {
     assert.strictEqual(fix(''), '');
     assert.strictEqual(fix('{}'), '{}');
     assert.strictEqual(fix('[]'), '[]');
+    assert.strictEqual(fix('{'), '');
+    assert.strictEqual(fix('}'), '');
+    assert.strictEqual(fix('['), '');
+    assert.strictEqual(fix(']'), '');
     assert.strictEqual(fix(':'), '');
     assert.strictEqual(fix('{"a": 1}'), '{"a":1}');
     assert.strictEqual(fix('{"a":: 1}'), '{"a":1}');
