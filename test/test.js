f68cf3b246a9f7a3f68893556c1a702cfc4f7cba
wuyu <wuyu.code@gmail.com>
2024-04-13 18:17:07 +0800

    Remove all unnecessary whitespace characters in JSON

--- test/test.js
+++ test/test.js
@@ -5,11 +5,11 @@ function testFix() {
     assert.strictEqual(fix(''), '');
     assert.strictEqual(fix('{}'), '{}');
     assert.strictEqual(fix('[]'), '[]');
-    assert.strictEqual(fix('{"a": 1}'), '{"a": 1}');
-    assert.strictEqual(fix('{"a": 1]'), '{"a": 1}');
-    assert.strictEqual(fix('{"a": 1, {"b": 2]]'), '{"a": 1, {"b": 2}}');
-    assert.strictEqual(fix('{"a": 1, b: [2, “3"}]'), '{"a": 1, b: [2, "3"]}');
-    assert.strictEqual(fix('{"a": 1, b: [2, “3"}] // this is a comment'), '{"a": 1, b: [2, "3"]} ');
+    assert.strictEqual(fix('{"a": 1}'), '{"a":1}');
+    assert.strictEqual(fix('{"a": 1]'), '{"a":1}');
+    assert.strictEqual(fix('{"a": 1, {"b": 2]]'), '{"a":1,{"b":2}}');
+    assert.strictEqual(fix('{"a": 1, b: [2, “3"}]'), '{"a":1,b:[2,"3"]}');
+    assert.strictEqual(fix('{"a": 1, b: [2, “3"}] // this is a comment'), '{"a":1,b:[2,"3"]}');
 }
 
 testFix();
