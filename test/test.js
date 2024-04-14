db33f0e8144ba8d6d463972f67794c8127c68ccc
wuyu <wuyu.code@gmail.com>
2024-04-15 01:27:10 +0800

    Update README.md and test.js to better demonstrate usage.

--- test/test.js
+++ test/test.js
@@ -5,9 +5,9 @@ function testFix() {
     assert.strictEqual(fix(''), '');
     assert.strictEqual(fix('{}'), '{}');
     assert.strictEqual(fix('[]'), '[]');
-    assert.strictEqual(fix('true'), 'true');
-    assert.strictEqual(fix('false'), 'false');
-    assert.strictEqual(fix('null'), 'null');
+    assert.strictEqual(fix('tRue'), 'true');
+    assert.strictEqual(fix('FalSE'), 'false');
+    assert.strictEqual(fix('nULl'), 'null');
     assert.strictEqual(fix('{'), '');
     assert.strictEqual(fix('}'), '');
     assert.strictEqual(fix('['), '');
@@ -33,7 +33,68 @@ function testFix() {
     assert.strictEqual(fix('{"a": 1, {"b": 2]]'), '{"a":1,{"b":2}}');
     assert.strictEqual(fix('{,,,"a",,:, 1,,, {,,,"b",: 2,,,],,,],,,'), '{"a":1,{"b":2}}');
     assert.strictEqual(fix('{"a": 1, b: [2, “3”:}]'), '{"a":1,"b":[2,"3"]}');
+    assert.strictEqual(fix('},{「a」:1,,b:[2,,“3”:},]},'), '{"a":1,"b":[2,"3"]}');
+    assert.strictEqual(fix('["quotes in "quotes" in quotes"]'), '["quotes in \\"quotes\\" in quotes"]');
     assert.strictEqual(fix('{"a": 1, b:: [2, “3":}] // this is a comment'), '{"a":1,"b":[2,"3"]}');
     assert.strictEqual(fix('},{,key:：/*multiline\ncomment\nhere*/ “//value",】， // this is an abnormal JSON'), '{"key":"//value"}');
 }
 testFix();
+
+function testFix1() {
+    // assert.strictEqual(fix("{ test: 'this is a test', 'number': 1.23e10 }"), '{"test":"this is a test","number":1.23e10}');
+}
+testFix1();
+
+function testFix2() {
+    assert.strictEqual(fix('{"test": "some text "a quote" more text"}'), '{"test":"some text \\"a quote\\" more text"}');
+}
+testFix2();
+
+function testFix3() {
+    assert.strictEqual(fix('{"test": "each \n on \n new \n line"}'), '{"test":"each \\n on \\n new \\n line"}');
+}
+testFix3();
+
+function testJsonDataWithComments() {
+    const jsonDataWithComments = `
+        {
+            // This is a comment
+            "name": "John",
+            "age": 30
+        }
+    `;
+    assert.strictEqual(fix(jsonDataWithComments), '{"name":"John","age":30}');
+}
+testJsonDataWithComments();
+
+function testJsonDataWithCommas() {
+    const jsonDataWithComments = `
+        {
+            "name": "John",
+            "age": 30, // Notice this trailing comma
+        }
+    `;
+    assert.strictEqual(fix(jsonDataWithComments), '{"name":"John","age":30}');
+}
+testJsonDataWithCommas();
+
+function testJsonDataWithMismatch() {
+    const jsonDataWithMismatch = `
+        {
+            "name": "John",
+            "age": 30,
+            "friends": [
+                "Alice",
+                "Bob",
+            } // this '}' should be ']'
+        】// this abnormal square bracket  should be '}'
+    `;
+    assert.strictEqual(fix(jsonDataWithMismatch), '{"name":"John","age":30,"friends":["Alice","Bob"]}');
+}
+testJsonDataWithMismatch();
+
+function testImproperlyWrittenJSON() {
+    const improperlyWrittenJSON = '},{「a」:1,,b:[2,,“3”:},]},';
+    assert.strictEqual(fix(improperlyWrittenJSON), '{"a":1,"b":[2,"3"]}');
+}
+testImproperlyWrittenJSON();
